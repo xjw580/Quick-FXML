5cd646d4ae3ca139561c13be763378e6e02e6ad5
GitHub Action <action@github.com>
2024-04-21 09:00:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/xjw580/quickfxml/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/xjw580/quickfxml/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.xjw580.quickfxml.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/xjw580/quickfxml/services/MyProjectService.kt
+++ src/main/kotlin/com/github/xjw580/quickfxml/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.xjw580.quickfxml.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xjw580.quickfxml.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
